b07f0dc6e6e644e57d7722cebe0d2ac46cdeba71
Chloe Quinto <cquinto@stevens.edu>
2018-03-19 10:21:10 -0400

    part 2

--- average.asm
+++ average.asm
@@ -16,4 +16,36 @@ loop: addd  2, Y+       ;add to accum D with [Y], post increment by 2
       
       
       
+;Now assume that DATA is a 24 element array of signed integers. Modify your code to give the average of the absolute
+;values of these integers
+
+;TO DO:
+;Check if it is negative, if so negate. Otherwise load into accum D
+
+;When testing a signed or unsinged you need to have two instructions a COMPARE and a TEST 
+
+
+      ldx   #25
+      ldy   #DATA
+      dex               ;decrement [x] by 1    
+loop: ldd  2,Y+         ;add values to accum D 
+      cpd   #0          ;compare value to zero 
+      bge   POS         ;branch if value is positive
+      nega              ;otherwise negate MSB
+      negb              ;otherwise negate LSB
+      std   2,Y+        ;store values in accum D 
+POS:  std   2,Y+
+      dbne  X, loop
+      ldx   #25
+      ldy   #DATA
+loop2: addd  2,Y+
+      dbne  X, loop2
+      addd  #12
+      ldx   #24
+      idivs             ;signed division  
+      stx   RESULT
+      swi
+      
+      
+      
       
